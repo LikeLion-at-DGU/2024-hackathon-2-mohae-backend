04f84b8b816aa54813b6a2b933a64a0225bb1f7a
whandrea <whandrea@naver.com>
2024-07-28 17:30:15 +0900

    feat.admin 관리

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Album, Photo, Video, PhotoVideoLike, Comment, Favorite
 # Register your models here.
+
+
+admin.site.register(Album)
+admin.site.register(Photo)
+admin.site.register(Video)
+admin.site.register(PhotoVideoLike)
+admin.site.register(Comment)
+admin.site.register(Favorite)

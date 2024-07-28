fd74e0b4b9dd97f22e47d2cd6f3f9d871e33ac76
whandrea <whandrea@naver.com>
2024-07-28 17:30:15 +0900

    feat.admin 관리

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Family, FamilyInvitation, BucketList 
 # Register your models here.
+
+admin.site.register(Family)
+admin.site.register(FamilyInvitation)
+admin.site.register(BucketList)

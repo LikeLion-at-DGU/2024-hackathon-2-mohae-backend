38b0f851ccb8c7288b9dce2dfbd6048f34ba6dd3
forestsoob <vini20022203@gmail.com>
2024-07-30 20:40:03 +0900

    add: admin.py

--- health/admin.py
+++ health/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Challenge)
+admin.site.register(Appointment)
+admin.site.register(Supplement)
+admin.site.register(Medication)

12bd44fcc0fd34c31659520931844861db293f70
forestsoob <vini20022203@gmail.com>
2024-07-31 02:43:28 +0900

    fix: cal

--- cal/models.py
+++ cal/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 from users.models import Family
-import uuid
-# Create your models here.
+from django.utils import timezone
 
 class Calendar(models.Model):
+    event_id = models.AutoField(primary_key=True)  # 일정 인덱스 (Primary Key)
+    title = models.CharField(max_length=255, null=False)  # 일정 제목
+    start = models.DateTimeField(null=False)  # 일정 시작 시간
+    end = models.DateTimeField(default=timezone.now, null=False)  # 일정 종료 시간
+    participants = models.ManyToManyField(User, related_name='calendar_events', blank=True)  # 참여자 (ManyToManyField로 변경)
+    emoji = models.TextField(null=True, blank=True)  # 이모지 필드 추가
+    emoji_text = models.TextField(null=True, blank=True)  # 이모지 텍스트 필드 추가
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')  # 생성자 (User 테이블 참조)
+    family_id = models.ForeignKey(Family, on_delete=models.CASCADE)  # 가족 인덱스 (Family 테이블 참조)
+    created_at = models.DateTimeField(auto_now_add=True)  # 생성 시간 (자동 설정)
+    updated_at = models.DateTimeField(auto_now=True)  # 수정 시간 (자동 설정)
+    status = models.CharField(max_length=1, default='Y', null=False)  # 상태 ('Y': 활성, 'N': 비활성)
 
-        event_id = models.AutoField(primary_key=True)  # 일정 인덱스 (Primary Key)
-        title = models.CharField(max_length=255, null=False)  # 일정 제목
-        description = models.TextField(null=True, blank=True)  # 일정 설명 (옵션)
-        type = models.CharField(max_length=255, null=False)  # 일정 유형
-        reminder_time = models.DateTimeField(null=True, blank=True)  # 알림 시간 (옵션)
-        created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 생성자 (User 테이블 참조)
-        family_id = models.ForeignKey(Family, on_delete=models.CASCADE)  # 가족 인덱스 (Family 테이블 참조)
-        created_at = models.DateTimeField(auto_now_add=True)  # 생성 시간 (자동 설정)
-        updated_at = models.DateTimeField(auto_now=True)  # 수정 시간 (자동 설정)
-        status = models.CharField(max_length=1, default='Y', null= False)  # 상태 ('Y': 활성, 'N': 비활성)
-
-        def __str__(self):
-            return self.title  # 객체를 문자열로 표현할 때 일정 제목 반환
+    def __str__(self):
+        return self.title  # 객체를 문자열로 표현할 때 일정 제목 반환
--- cal/urls.py
+++ cal/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('events/', views.create_event, name='create_event'),
+    path('events/list/', views.list_events, name='list_events'),  # 이벤트 목록 조회 추가
     path('events/<int:pk>/', views.event_detail, name='event_detail'),
     path('events/<int:pk>/update/', views.update_event, name='update_event'),
     path('events/<int:pk>/delete/', views.delete_event, name='delete_event'),

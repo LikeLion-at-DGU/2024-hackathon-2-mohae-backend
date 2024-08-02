a6aef75fef4532fbeebb1cd2acde00f491449fdd
whandrea <whandrea@naver.com>
2024-08-03 05:17:14 +0900

    fix. cal 가족 자동 업데이트

--- cal/utils.py
+++ cal/utils.py
@@ -1,6 +1,11 @@
-from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 from users.models import Family
 
+User = get_user_model()
+
 def is_user_family_member(user, family_id):
-    family = get_object_or_404(Family, pk=family_id)
-    return user in family.created_by.families.all()
+    try:
+        family = Family.objects.get(pk=family_id)
+        return user.profile.family == family
+    except Family.DoesNotExist:
+        return False
--- cal/views.py
+++ cal/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from accounts.models import Profile
 from .utils import is_user_family_member
 
 User = get_user_model()
@@ -16,8 +17,14 @@ User = get_user_model()
 def create_event(request):
     serializer = CalendarSerializer(data=request.data, context={'request': request})
     if serializer.is_valid():
-        family_id = request.data.get('family_id')
-        family = get_object_or_404(Family, pk=family_id)  # family_id가 유효한지 확인
+        # 현재 사용자의 가족 ID 가져오기
+        user_profile = Profile.objects.get(user=request.user)
+        family = user_profile.family
+        if family is None:
+            return Response({"detail": "Family is not set for the user's profile."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # family_id가 유효한지 확인
+        family_id = family.pk
 
         # 현재 사용자가 가족 구성원인지 확인
         if not is_user_family_member(request.user, family_id):
@@ -27,7 +34,7 @@ def create_event(request):
         
         # 참가자 목록을 가족 구성원으로 제한
         participants_ids = request.data.get('participants', [])
-        participants = User.objects.filter(id__in=participants_ids, families__family_id=family_id)
+        participants = User.objects.filter(id__in=participants_ids, profile__family=family_id)
         event.participants.set(participants)  # participants 설정
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -54,7 +61,7 @@ def update_event(request, pk):
     serializer = CalendarSerializer(event, data=request.data, partial=True)
     if serializer.is_valid():
         participants_ids = request.data.get('participants', [])
-        participants = User.objects.filter(id__in=participants_ids, families__family_id=event.family_id.family_id)
+        participants = User.objects.filter(id__in=participants_ids, profile__family=event.family_id)
         event.participants.set(participants)  # participants 설정
 
         serializer.save()
@@ -74,9 +81,9 @@ def family_members(request, family_id):
     family = get_object_or_404(Family, pk=family_id)
     
     # 현재 사용자가 가족 구성원인지 확인
-    if request.user not in family.created_by.families.all():
+    if not is_user_family_member(request.user, family_id):
         return Response({"detail": "You are not a member of this family."}, status=status.HTTP_403_FORBIDDEN)
     
-    members = family.created_by.families.all()
+    members = User.objects.filter(profile__family=family)
     serializer = UserSerializer(members, many=True)
     return Response(serializer.data)

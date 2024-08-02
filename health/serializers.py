c1a4055bea4e5a429a8fd3aadbf51d0869ad7d8b
forestsoob <vini20022203@gmail.com>
2024-08-02 19:25:01 +0900

    fix: health

--- health/serializers.py
+++ health/serializers.py
@@ -5,6 +5,7 @@ class MedicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medication
         fields = '__all__'
+        read_only_fields = ('user',)  # user 필드를 읽기 전용으로 설정
 
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:

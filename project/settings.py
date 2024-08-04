ec0aea3dff6ff0ca2c440ace0620cd8f797d1b30
whandrea <whandrea@naver.com>
2024-08-04 19:21:11 +0900

    fix. settings

--- project/settings.py
+++ project/settings.py
@@ -86,18 +86,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
 # 로컬 개발용 주소
-'http://localhost:3000',
-'http://localhost:5173',
-'http://localhost:5174',
-'http://127.0.0.1:3000',
-'http://127.0.0.1:5173',
-'http://127.0.0.1:5174',
-# 프론트엔드 도메인 또는 IP주소
-# 예를 들어, 아래와 같이 입력.
-'http://13.209.61.181',
-'http://13.209.61.181:5173',
-'http://13.209.61.181:5174',
-'http://3.35.44.88:8000',
+'*'
 ]
 
 REST_FRAMEWORK = {

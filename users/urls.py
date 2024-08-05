d4730a97a1c051f096bae67447d64b7551449b26
whandrea <whandrea@naver.com>
2024-08-06 02:52:45 +0900

    users 가족

--- users/urls.py
+++ users/urls.py
@@ -6,9 +6,9 @@ from .views import BucketListViewSet, MyPageViewSet, FamilyViewSet, ProfileViewS
 app_name = 'users'
 
 router = DefaultRouter(trailing_slash=False)
-router.register('bucketlists', BucketListViewSet)
-router.register('family', FamilyViewSet)
-router.register('profiles', ProfileViewSet, basename='profile')
+router.register('bucketlists', BucketListViewSet, basename='bucketlists')
+router.register('family', FamilyViewSet, basename='family')
+router.register('profiles', ProfileViewSet, basename='profiles')
 
 mypage_router = DefaultRouter(trailing_slash=False)
 mypage_router.register('mypage', MyPageViewSet, basename='mypage')
@@ -16,6 +16,6 @@ mypage_router.register('mypage', MyPageViewSet, basename='mypage')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(mypage_router.urls)),
-    path('family/join/', FamilyViewSet.as_view({'post': 'join_by_code'})),
-    path('invite/', invite_family_member, name='invite-family-member'),
+    path('family/join/', FamilyViewSet.as_view({'post': 'join_by_code'})), # 가족 코드로 가입
+    path('invite/', invite_family_member, name='invite-family-member'),  # 초대 코드 발급 및 SMS 발송 엔드포인트 추가
 ]

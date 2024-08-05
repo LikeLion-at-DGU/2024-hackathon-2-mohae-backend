61e183110dddb5a7250cdb3f737caa463cfaf0a2
whandrea <whandrea@naver.com>
2024-08-06 04:13:20 +0900

    users 수정

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
+# urls.py
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BucketListViewSet, MyPageViewSet, FamilyViewSet, ProfileViewSet, invite_family_member
-
 
 app_name = 'users'
 
@@ -16,6 +17,6 @@ mypage_router.register('mypage', MyPageViewSet, basename='mypage')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(mypage_router.urls)),
-    path('family/join/', FamilyViewSet.as_view({'post': 'join_by_code'})), # 가족 코드로 가입
+    path('family/join/', FamilyViewSet.as_view({'post': 'join_by_code'}),name='join-by-code'), # 가족 코드로 가입
     path('invite/', invite_family_member, name='invite-family-member'),  # 초대 코드 발급 및 SMS 발송 엔드포인트 추가
 ]
